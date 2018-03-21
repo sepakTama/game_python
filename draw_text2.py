309cf82dbbb122e7048601bc5d8eee024c4e811c
sepakTama <ma.tama121096@gmail.com>
2018-03-21 17:23:06 +0900

    3/21

--- draw_text2.py
+++ draw_text2.py
@@ -6,24 +6,29 @@ import pygame
 from pygame.locals import QUIT
 
 pygame.init()
-SURFACE = pygame.display.set_mode((400,200))
+SURFACE = pygame.display.set_mode((400,300))
 FPSCLOCK = pygame.time.Clock()
 
 def main():
     sysfont = pygame.font.SysFont(None, 72)
     message = sysfont.render("Hello Python", True, (0,128,128))
     message_rect = message.get_rect()
-    message_rect.center = (200,100)
-
+    theta = 0
+    scale = 1
     while True:
         for event in pygame.event.get(QUIT):
             pygame.quit()
             sys.exit()
 
         SURFACE.fill((255,255,255))
-        SURFACE.blit(message, message_rect)
+        theta += 5
+        scale = (theta % 360)/180
+        tmp_msg = pygame.transform.rotozoom(message, theta, scale)
+        tmp_rect = tmp_msg.get_rect()
+        tmp_rect.center = (200, 150)
+        SURFACE.blit(tmp_msg, tmp_rect)
         pygame.display.update()
-        FPSCLOCK.tick(3)
+        FPSCLOCK.tick(10)
 
 if __name__ == '__main__':
     main()
